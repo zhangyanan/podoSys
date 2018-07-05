26c5d7121413fb2a27fedfd229e9178ce0ef2dd1
ivy <ivyzhang_happy@163.com>
2018-07-05 16:04:20 +0800

    change app.vue

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,13 @@
       <f7-block-title>我是左侧面板</f7-block-title>
       <f7-block>
         <f7-link panel-close>close me</f7-link>
-        <p>左侧面板</p>
+        <p>左侧面板 link: . You can put here anything, even another isolated view like in  <f7-link panel-open="right">Right Panel</f7-link></p>
       </f7-block>
     </f7-panel>
     <f7-view :pushState="true" main/>
     <v-menu></v-menu>
   </div>
 </template>
-
 <script>
 export default {
   name: 'App'
