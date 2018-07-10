3a43f758bd21ea5ecee126637e99127bab21ea66
ivy <ivyzhang_happy@163.com>
2018-07-10 17:28:43 +0800

    replace wheatlist page because hasn't use f7 control

---
+++ src/pages/wheat/gglibrary.vue
@@ -0,0 +1,38 @@
+<template>
+<f7-page>
+  <v-asideheader title=''></v-asideheader>
+  <f7-list media-list class="gglibrary">
+    <f7-list-item link="#" title="赵鸿祎" after="SW" subtitle="zhaohongwei1990" text="pulvinar lacus.">
+      <img slot="media" src="@/assets/icon_all/shizi.png" width="59"/>
+    </f7-list-item>
+    <f7-list-item link="#" title="宋美月" after="ST" subtitle="lucyQueen" text="pulvinar lacus.">
+      <img slot="media" src="@/assets/icon_all/shizi.png" width="59"/>
+    </f7-list-item>
+    <f7-list-item link="#" title="刘曾涛" after="FF" subtitle="taoTao231" text="pulvinar lacus.">
+      <img slot="media" src="@/assets/icon_all/shizi.png" width="59"/>
+    </f7-list-item>
+    <f7-list-item link="#" title="吴萌萌" after="CJ" subtitle="wumengmeng" text="pulvinar lacus.">
+      <img slot="media" src="@/assets/icon_all/shizi.png" width="59"/>
+    </f7-list-item>
+    <f7-list-item link="#" title="王恩慈" after="BK" subtitle="MichaelWang" text="pulvinar lacus.">
+      <img slot="media" src="@/assets/icon_all/shizi.png" width="59"/>
+    </f7-list-item>
+  </f7-list>
+</f7-page>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+div.list.media-list.gglibrary{
+  margin: 0px;
+}
+div.navbar-inner.sliding{
+  height: 65px;
+  background: #FFFFFF;
+}
+</style>
