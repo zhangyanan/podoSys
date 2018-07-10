4b106d8279747113c53410c6f4cd137b3c00fcbd
ivy <ivyzhang_happy@163.com>
2018-07-10 23:50:32 +0800

    replace alllist page

--- src/pages/wheat/gglibrary.vue
+++ src/pages/wheat/gglibrary.vue
@@ -2,21 +2,227 @@
 <f7-page>
   <v-asideheader title=''></v-asideheader>
   <f7-list media-list class="gglibrary">
-    <f7-list-item link="#" title="赵鸿祎" after="SW" subtitle="zhaohongwei1990" text="pulvinar lacus.">
-      <img slot="media" src="@/assets/icon_all/shizi.png" width="59"/>
+    <f7-list-item link="#" class="sw-bg">
+       <div slot="subtitle" class="ggname">赵鸿祎</div>         
+       <div slot="media">
+         <div class="name-bg-sw"><span class="surname">赵</span></div>
+       </div>
+      <div slot="subtitle">
+        <div class="row">
+            <div class="col-50">zhaohongwei1990</div>
+            <div class="col-50">
+              <div class="item-title-row">
+                <div class="chip color-red">
+                    <div class="chip-label">SW</div>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+       <div slot="text">
+          <div class="row nogap">
+              <div class="col-40">
+                <div class="row">
+                  <div class="col-20"><span class="phone-icon-red"><img src="@/assets/icon_all/listicon_SW1.png"/></span></div>
+                  <div class="col-80"><span class="phone">13524677323</span></div>
+                </div>
+              </div>
+              <div class="col-20">
+                <div class="row">
+                  <div class="col-50"><span class="gender-icon-red"><img src="@/assets/icon_all/listicon_SW2.png"/></span></div>
+                  <div class="col-50"><span class="gender">女</span></div>
+                </div>
+              </div>
+              <div class="col-40">
+                <div class="row">
+                  <div class="col-20"><span class="date-icon-red"><img src="@/assets/icon_all/listicon_SW3.png"/></span></div>
+                  <div class="col-80"><span class="date">2018.08.10</span></div>
+                </div>
+              </div>
+          </div>
+       </div>
+       <div slot="root-end">
+          <div class="div-border"></div>
+       </div>
     </f7-list-item>
-    <f7-list-item link="#" title="宋美月" after="ST" subtitle="lucyQueen" text="pulvinar lacus.">
-      <img slot="media" src="@/assets/icon_all/shizi.png" width="59"/>
+
+    <f7-list-item link="#" class="st-bg">
+       <div slot="subtitle" class="ggname">宋美月</div>         
+       <div slot="media">
+         <div class="name-bg-st"><span class="surname">宋</span></div>
+       </div>
+      <div slot="subtitle">
+        <div class="row">
+            <div class="col-50">lucyQueen</div>
+            <div class="col-50">
+              <div class="item-title-row">
+                <div class="chip color-green">
+                    <div class="chip-label">ST</div>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+       <div slot="text">
+          <div class="row nogap">
+              <div class="col-40">
+                <div class="row">
+                  <div class="col-20"><span class="phone-icon-green"><img src="@/assets/icon_all/listicon_green1.png"/></span></div>
+                  <div class="col-80"><span class="phone">13524677323</span></div>
+                </div>
+              </div>
+              <div class="col-20">
+                <div class="row">
+                  <div class="col-50"><span class="gender-icon-green"><img src="@/assets/icon_all/listicon_green2.png"/></span></div>
+                  <div class="col-50"><span class="gender">女</span></div>
+                </div>
+              </div>
+              <div class="col-40">
+                <div class="row">
+                  <div class="col-20"><span class="date-icon-green"><img src="@/assets/icon_all/listicon_green3.png"/></span></div>
+                  <div class="col-80"><span class="date">2018.08.10</span></div>
+                </div>
+              </div>
+          </div>
+       </div>
+       <div slot="root-end">
+          <div class="div-border"></div>
+       </div>
     </f7-list-item>
-    <f7-list-item link="#" title="刘曾涛" after="FF" subtitle="taoTao231" text="pulvinar lacus.">
-      <img slot="media" src="@/assets/icon_all/shizi.png" width="59"/>
+
+    <f7-list-item link="#" class="ff-bg">
+       <div slot="subtitle" class="ggname">刘曾涛</div>         
+       <div slot="media">
+         <div class="name-bg-ff"><span class="surname">刘</span></div>
+       </div>
+      <div slot="subtitle">
+        <div class="row">
+            <div class="col-50">taoTao231</div>
+            <div class="col-50">
+              <div class="item-title-row">
+                <div class="chip color-orange">
+                    <div class="chip-label">FF</div>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+       <div slot="text">
+          <div class="row nogap">
+              <div class="col-40">
+                <div class="row">
+                  <div class="col-20"><span class="phone-icon-orange"><img src="@/assets/icon_all/listicon_orange1.png"/></span></div>
+                  <div class="col-80"><span class="phone">13524677323</span></div>
+                </div>
+              </div>
+              <div class="col-20">
+                <div class="row">
+                  <div class="col-50"><span class="gender-icon-orange"><img src="@/assets/icon_all/listicon_orange2.png"/></span></div>
+                  <div class="col-50"><span class="gender">女</span></div>
+                </div>
+              </div>
+              <div class="col-40">
+                <div class="row">
+                  <div class="col-20"><span class="date-icon-orange"><img src="@/assets/icon_all/listicon_orange3.png"/></span></div>
+                  <div class="col-80"><span class="date">2018.08.10</span></div>
+                </div>
+              </div>
+          </div>
+       </div>
+       <div slot="root-end">
+          <div class="div-border"></div>
+       </div>
     </f7-list-item>
-    <f7-list-item link="#" title="吴萌萌" after="CJ" subtitle="wumengmeng" text="pulvinar lacus.">
-      <img slot="media" src="@/assets/icon_all/shizi.png" width="59"/>
+
+
+    <f7-list-item link="#" class="cj-bg">
+       <div slot="subtitle" class="ggname">吴萌萌</div>         
+       <div slot="media">
+         <div class="name-bg-cj"><span class="surname">吴</span></div>
+       </div>
+      <div slot="subtitle">
+        <div class="row">
+            <div class="col-50">zhaohongwei1990</div>
+            <div class="col-50">
+              <div class="item-title-row">
+                <div class="chip color-blue">
+                    <div class="chip-label">CJ</div>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+       <div slot="text">
+          <div class="row nogap">
+              <div class="col-40">
+                <div class="row">
+                  <div class="col-20"><span class="phone-icon-blue"><img src="@/assets/icon_all/listicon_blue1.png"/></span></div>
+                  <div class="col-80"><span class="phone">13524677323</span></div>
+                </div>
+              </div>
+              <div class="col-20">
+                <div class="row">
+                  <div class="col-50"><span class="gender-icon-blue"><img src="@/assets/icon_all/listicon_blue2.png"/></span></div>
+                  <div class="col-50"><span class="gender">女</span></div>
+                </div>
+              </div>
+              <div class="col-40">
+                <div class="row">
+                  <div class="col-20"><span class="date-icon-blue"><img src="@/assets/icon_all/listicon_blue3.png"/></span></div>
+                  <div class="col-80"><span class="date">2018.08.10</span></div>
+                </div>
+              </div>
+          </div>
+       </div>
+       <div slot="root-end">
+          <div class="div-border"></div>
+       </div>
     </f7-list-item>
-    <f7-list-item link="#" title="王恩慈" after="BK" subtitle="MichaelWang" text="pulvinar lacus.">
-      <img slot="media" src="@/assets/icon_all/shizi.png" width="59"/>
+
+    <f7-list-item link="#" class="gray-bg">
+       <div slot="subtitle" class="ggname">王恩慈</div>         
+       <div slot="media">
+         <div class="name-bg-gray"><span class="surname">王</span></div>
+       </div>
+      <div slot="subtitle">
+        <div class="row">
+            <div class="col-50">zhaohongwei1990</div>
+            <div class="col-50">
+              <div class="item-title-row">
+                <div class="chip color-gray">
+                    <div class="chip-label">BK</div>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+       <div slot="text">
+          <div class="row nogap">
+              <div class="col-40">
+                <div class="row">
+                  <div class="col-20"><span class="phone-icon-gray"><img src="@/assets/icon_all/listicon_black1.png"/></span></div>
+                  <div class="col-80"><span class="phone">13524677323</span></div>
+                </div>
+              </div>
+              <div class="col-20">
+                <div class="row">
+                  <div class="col-50"><span class="gender-icon-gray"><img src="@/assets/icon_all/listicon_gray2.png"/></span></div>
+                  <div class="col-50"><span class="gender">女</span></div>
+                </div>
+              </div>
+              <div class="col-40">
+                <div class="row">
+                  <div class="col-20"><span class="date-icon-gray"><img src="@/assets/icon_all/listicon_gray3.png"/></span></div>
+                  <div class="col-80"><span class="date">2018.08.10</span></div>
+                </div>
+              </div>
+          </div>
+       </div>
+       <div slot="root-end">
+          <div class="div-border"></div>
+       </div>
     </f7-list-item>
+
   </f7-list>
 </f7-page>
 </template>
@@ -28,6 +234,7 @@ export default {
 </script>
 
 <style>
+
 div.list.media-list.gglibrary{
   margin: 0px;
 }
@@ -35,4 +242,294 @@ div.navbar-inner.sliding{
   height: 65px;
   background: #FFFFFF;
 }
+div.list.gglibrary.media-list ul li{
+  border: 0px solid #E9E9E9;
+  height: 95px;
+}
+label.firstcharacter{
+  margin-top: -20px;
+  margin-left: -20px;
+  font-family: PingFangSC-Semibold;
+  font-size: 24px;
+  color: #FFFFFF;
+  letter-spacing: 0;
+  line-height: 21px;
+}
+div.ggname
+{
+  font-family: PingFangSC-Semibold;
+  font-size: 14px;
+  color: #4A4A4A;
+  letter-spacing: 0;
+  line-height: 21px;
+}
+div.item-subtitle{
+  font-family: PFSquareSansPro-Light;
+  font-size: 15px;
+  color: #4A4A4A;
+  letter-spacing: 0;
+  line-height: 18px;
+}
+.md .list li.sw_bg .item-after{
+  background: #FF6D7F;
+  border-radius: 32.4px;
+  width: 22px;
+  height: 15px;
+}
+li.sw_bg div.item-after span{
+  font-family: PFSquareSansPro-Bold;
+  font-size: 8px;
+  color: #FFFFFF;
+  letter-spacing: 0;
+  line-height: 15px;
+}
+.md .list li.st_bg .item-after{
+  background: #54BCBF;;
+  border-radius: 32.4px;
+  width: 22px;
+  height: 15px;
+}
+li.st_bg div.item-after span{
+  font-family: PFSquareSansPro-Bold;
+  font-size: 8px;
+  color: #FFFFFF;
+  letter-spacing: 0;
+  line-height: 15px;
+}
+.md .list li.ff_bg .item-after{
+  background: #EF9F62;;
+  border-radius: 32.4px;
+  width: 22px;
+  height: 15px;
+}
+li.ff_bg div.item-after span{
+  font-family: PFSquareSansPro-Bold;
+  font-size: 8px;
+  color: #FFFFFF;
+  letter-spacing: 0;
+  line-height: 15px;
+}
+.md .list li.cj_bg .item-after{
+  background: #5C9FC9;;
+  border-radius: 32.4px;
+  width: 22px;
+  height: 15px;
+}
+li.cj_bg div.item-after span{
+  font-family: PFSquareSansPro-Bold;
+  font-size: 8px;
+  color: #FFFFFF;
+  letter-spacing: 0;
+  line-height: 15px;
+}
+.md .list li.bk_bg .item-after{
+  background: #9F9F9F;;
+  border-radius: 32.4px;
+  width: 22px;
+  height: 15px;
+}
+li.bk_bg div.item-after span{
+  font-family: PFSquareSansPro-Bold;
+  font-size: 8px;
+  color: #FFFFFF;
+  letter-spacing: 0;
+  line-height: 15px;
+}
+span.phone-icon-red,span.phone-icon-green,span.phone-icon-orange,span.phone-icon-blue,span.phone-icon-gray{
+    padding-right: 2px;
+  }
+  span.phone-icon-red img{
+    width:14px;
+    height: 14px;
+    padding-top: 2px;
+  }
+  span.phone-icon-green img{
+    width:14px;
+    height: 14px;
+    padding-top: 2px;
+  }
+  span.phone-icon-orange img{
+    width:14px;
+    height: 14px;
+    padding-top: 2px;
+  }
+  span.phone-icon-blue img{
+    width:14px;
+    height: 14px;
+    padding-top: 2px;
+  }
+  span.phone-icon-gray img{
+    width:14px;
+    height: 14px;
+    padding-top: 2px;
+  }
+ .row .col-80 .phone{
+    font-family: PFSquareSansPro-Regular;
+    font-size: 12px;
+    color: #4A4A4A;
+    letter-spacing: -0.48px;
+    line-height: 18px;
+    margin-left: -2px;
+  }
+  span.gender-icon-red,span.gender-icon-green,span.gender-icon-orange,span.gender-icon-blue,span.gender-icon-gray{
+    padding-right: 2px;
+  }
+  span.gender-icon-red img{
+    width:14px;
+    height: 14px;
+    padding-top: 2px;
+  }
+  span.gender-icon-green img{
+    width:14px;
+    height: 14px;
+    padding-top: 2px;
+  }
+  span.gender-icon-orange img{
+    width:14px;
+    height: 14px;
+    padding-top: 2px;
+  }
+  span.gender-icon-blue img{
+    width:14px;
+    height: 14px;
+    padding-top: 2px;
+  }
+  span.gender-icon-gray img{
+    width:14px;
+    height: 14px;
+    padding-top: 2px;
+  }
+  .row .col-50 .gender{
+    font-family: PingFangSC-Regular;
+    font-size: 10px;
+    color: #4A4A4A;
+    letter-spacing: -0.48px;
+    line-height: 18px;
+    margin-left: -6px;
+  }
+  span.date-icon-red, span.date-icon-green,span.gender-icon-orange,span.gender-icon-blue,span.gender-icon-gray{
+    padding-right: 2px;
+  }
+  span.date-icon-red img{
+    width: 15px;
+    height: 15px;
+    padding-top: 2px;
+  }
+  span.date-icon-green img{
+    width: 15px;
+    height: 15px;
+    padding-top: 2px;
+  }
+  span.date-icon-orange img{
+    width: 15px;
+    height: 15px;
+    padding-top: 2px;
+  }
+  span.date-icon-blue img{
+    width: 15px;
+    height: 15px;
+    padding-top: 2px;
+  }
+  span.date-icon-gray img{
+    width: 15px;
+    height: 15px;
+    padding-top: 2px;
+  }
+  .row .col-80 .date{
+    font-family: PFSquareSansPro-Regular;
+    font-size: 12px;
+    color: #4A4A4A;
+    letter-spacing: -0.48px;
+    line-height: 18px;
+    margin-left: -3px;
+  }
+  .md .chip {
+    font-size: 13px;
+    height: 15.5px;
+    line-height: 32px;
+    width: 38.5px;
+    border-radius: 16px;
+    padding: 0 8px;
+    margin-left: 60px;
+}
+.md .chip.color-red {
+    background: #FF6D7F;
+    border-radius: 32.4px;
+}
+.md .chip.color-green {
+    background: #54BCBF;
+    border-radius: 32.4px;
+}
+.md .chip.color-blue {
+    background: #5C9FC9;
+    border-radius: 32.4px;
+}
+.md .chip.color-orange {
+    background: #EF9F62;
+    border-radius: 32.4px;
+}
+.md .chip.color-gray {
+    background: #9D9D9D;
+    border-radius: 32.4px;
+}
+.item-title-row .chip-label{
+    font-family: PFSquareSansPro-Bold;
+    font-size: 11px;
+    color: #FFFFFF;
+    letter-spacing: 0;
+    font-weight: 600;
+    line-height: 17px;
+    padding-left: 2px;
+}
+
+  .name-bg-sw{
+    background-color: #FF6D7F;
+    width:59px;
+    height: 59px;
+    line-height: 59px;
+    border-radius: 30px;
+    display:table-cell;
+  }
+  .name-bg-st{
+    background-color: #54BCBF;
+    width:59px;
+    height: 59px;
+    line-height: 59px;
+    border-radius: 30px;
+    display:table-cell;
+  }
+  .name-bg-ff{
+    background-color: #EF9F62;
+    width:59px;
+    height: 59px;
+    line-height: 59px;
+    border-radius: 30px;
+    display:table-cell;
+  }
+  .name-bg-cj{
+    background-color: #5C9FC9;
+    width:59px;
+    height: 59px;
+    line-height: 59px;
+    border-radius: 30px;
+    display:table-cell;
+  }
+  .name-bg-gray{
+    background-color: #9F9F9F;
+    width:59px;
+    height: 59px;
+    line-height: 59px;
+    border-radius: 30px;
+    display:table-cell;
+  }
+ .surname{
+    color:white;
+    font-size: 24px;
+    font-family: PingFangSC-Semibold;
+    padding-left: 18px;
+  }
+  div.div-border{
+    padding-top: 1px;
+    background: #E9E9E9;
+  }
 </style>
