58e3835ce49dfd3e8eb9ac50d0e8f82209729098
ivy <ivyzhang_happy@163.com>
2018-07-10 17:27:27 +0800

    update asideheader to f7 tools

--- src/components/AsideHeader.vue
+++ src/components/AsideHeader.vue
@@ -2,25 +2,8 @@
   .md .navbar {
       height: 67px;
       color: #fff;
-      font-size: 20px;
   }
-  .pull-out-icon{
-    width: 28px;
-    height: 28px;
-  }
-  .pull-out-icon img{
-    width: 30px;
-    height: 30px;
-  }
-  .add_21profile-icon{
-    width: 28px;
-    height: 28px;
-  }
- .add_21profile-icon img{
-    width: 28px;
-    height: 28px;
-    
-  }
+ 
 
   .header {
     position: relative;
@@ -44,51 +27,24 @@
       right: 10px;
     }
   }
-  .md .button.button-raised {
-     -webkit-box-shadow: 0 0px 0px rgba(0,0,0,0), 0 0px 0px rgba(0,0,0,0);
-     box-shadow: 0 0px 0px rgba(0,0,0,0), 0 0px 0px rgba(0,0,0,0); 
-}
+    .md .navbar:after{
+      background: linear-gradient(to bottom,rgba(0,0,0,0) 0,rgba(0,0,0,0) 0,rgba(0,0,0,0) 0,rgba(0,0,0,0) 0,rgba(0,0,0,0) 0); 
+    }
 </style>
 <template>
   <f7-navbar>
-    <f7-nav-left>
-    </f7-nav-left>
-    <f7-nav-title>
-      <label class="site-title">
-        <f7-link href="/myItem/" no-link-class>
-          <span class="pull-out-icon">
-            <f7-button raised panel-open="left"><img src="@/assets/icon_all/pull_out@2x.png"/></f7-button>
-          </span>
-      </label>
-      <label class="site-title">|</label>
-      <label class="content-title">{{title}}</label>
-    </f7-nav-title>
-    <f7-nav-right>
-      <f7-link addPage v-if="addPage" href="/quick/">
-        <span class="add_21profile-icon">
-          <img src="@/assets/icon_all/add_21profile@2x.png"/>
-        </span>
-      </f7-link>
-      <f7-link icon-material="menu" panel-open="right" v-else-if="!noMenu" popup-open data-popup=".popup-menu"></f7-link>
-    </f7-nav-right>
-  </f7-navbar>
+  <f7-nav-left>
+    <f7-link href="/myItem/" raised panel-open="left"><img src="@/assets/icon_all/pull_out.png"/></f7-link>
+  </f7-nav-left>
+  <f7-nav-title></f7-nav-title>
+  <f7-nav-right>
+    <!-- <f7-link addPage v-if="addPage" href="/quick/"><img src="@/assets/icon_all/add_21profile.png"/></f7-link>-->
+    <f7-link href="/quick/"><img src="@/assets/icon_all/add_21profile.png"/></f7-link>
+  </f7-nav-right>
+</f7-navbar>
 </template>
 <script>
 export default {
-  data() {
-    return {
-           }
-  },
-  computed: {
-  },
-  created () {
-    
-  },
-  methods: {
-    goleft(keyid, index){
-      this.$f7router.navigate('/myItem/')
-    },
-  },
   props: {
     title: {
       type: String,
