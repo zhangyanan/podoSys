3964cacd22234c07786ad00266afb19f36c747be
ivy <ivyzhang_happy@163.com>
2018-07-25 22:04:22 +0800

    修改样式

--- src/pages/dashboard/index.vue
+++ src/pages/dashboard/index.vue
@@ -1,27 +1,27 @@
 <template>
   <f7-page class="index-page">
-    <form class="index" v-on:submit.prevent="submit" no-hairlines-md>
+    <form class="login" v-on:submit.prevent="submit" no-hairlines-md>
       <f7-list class="welcome-list">
         <f7-list-item class="text-center b-0">
           <h1 class="white">WELCOME</h1>
         </f7-list-item>
-       <f7-list-item class="welcome text-center b-0">
+       <f7-list-item class="text-center b-0">
           <h1 class="yellow">欢迎回家</h1>
         </f7-list-item>
-        <f7-list-item class="home text-center b-0">
+        <f7-list-item class="text-center b-0">
           <h1 class="white">HOME</h1>
         </f7-list-item>
       </f7-list>
-      <f7-row class="smileform" no-gap>
+      <f7-row no-gap>
         <f7-col width="70">
           <f7-list class="smiler-list">
-            <f7-list-item><div class="item-text-smiler"><img src="@/assets/icon_all/smiler.png" /></div></f7-list-item>
+            <f7-list-item><div class="item-text"><img src="@/assets/icon_all/smiler.png" /></div></f7-list-item>
             <f7-list-item><div class="item-subtitle1">BEING A SMILER</div></f7-list-item>
             <f7-list-item><div class="item-subtitle2">永远微笑的活着</div></f7-list-item>
           </f7-list>
         </f7-col>
         <f7-col width="30">
-          <f7-list class="group-list">
+          <f7-list>
             <f7-list-item>
               <f7-link href="/inputform/"><img src="@/assets/icon_all/Group.png"/></f7-link>           
             </f7-list-item>
@@ -46,7 +46,7 @@
   div.page.index-page.page-current {
     background-image: url("../../assets/icon_all/login_bg.png")
   }
-  .md .index .list.welcome-list {
+  .md .login .list {
       margin: 0px;
   }
   .md div.list.smiler-list .item-content {
@@ -57,7 +57,7 @@
     margin-top: 74px;
     margin-left: 40px;
   }
-  .md .list .item-text-smiler{
+  .md .login .list .item-text{
     padding-top: 35px;
     padding-left: 35px;
   }
@@ -79,7 +79,7 @@
     padding-top: 45px;
     padding-left: 35px;
   }
-  .index {
+  .login {
     padding: 0px;
     text-align: center;
 
@@ -97,12 +97,10 @@
     h1.white{
       font-family: PF Square Sans Pro;
       color:white;
-      line-height: 25px;
     }
     h1.yellow{
       font-family: PingFangTC-Semibold;
       color:yellow;
-      line-height: 25px;
     }
     .line {
       padding: 5px;
@@ -112,24 +110,32 @@
         line-height: 28px;
       }
     }
+    .item-content {
+      border: 0px solid #eee;
+      margin-bottom: 0px;
+    }
+    .item-content-button {
+      .item-content {
+        margin-top: 30px;
+        border: 0px;
+      }
+    }
+    .button, .button.button-fill, .button.button-fill.active-state {
+      background-color: #3cc8b4;
+    }
   }
-  div.list.welcome-list ul:before, div.list.welcome-list ul:after,div.list.group-list ul:before,div.list.group-list ul:after {
+  .md .login .list ul:before, .md .login .list ul:after, .md .login .item-input-wrap:after, .md .login .list .item-inner:after {
     background-color: transparent;
   }
-  div.list.smiler-list ul:before,div.list.smiler-list ul:after,div.list.group-list ul:after{
-   background-color: transparent;
- }
- div.welcome.text-center.b-0.item-inner{
-   background-color: transparent;
- }
- .md .list.welcome-list .item-inner:after,.md .list.smiler-list .item-inner:after{
-   background-color: transparent;
- }
+
   div .login .row.no-gap {
       margin-top: 285px;
   }
-   .md .login .list ul:before, .md .login .list ul:after, .md .login .item-input-wrap:after, .md .login .list .item-inner:after {
-    background-color: transparent;
+  .md .row.no-gap .col-30 {
+   // padding-left: 35px;
+  }
+  .md .row.no-gap .col-70 {
+    //padding-left: 56px;
   }
   div.list.smiler-list ul li{
     height: 30px;
